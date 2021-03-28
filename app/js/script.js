c1c3ad7bce1cb2b825ad75a216c0ac5940bc8dda
tonyspunei <tony.spunei@yahoo.com>
2021-03-28 13:19:29 +0200

    done with the JS

---
+++ app/js/script.js
@@ -0,0 +1,26 @@
+const countDownDate = new Date("November 20, 2021 13:00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("container").innerHTML = `
+    <div class="expired">
+        <h1>EXPIRED</h1>
+    </div>`;
+  }
+}, 1000);
